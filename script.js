467b367251d4a45ec78ed86011df8f3de7f5c805
KarthikChillal <chillalkarthik@gmail.com>
2026-01-29 23:52:18 +0530

    fixed the check function.

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ function Player(name, marker){
   return {name, marker};
 }
 
+
+
+
+
 let gameBoard =  (function (){
   let board =['.', '.', '.', '.', '.', '.', '.', '.', '.', ];
   const mark = (marker, index)=>{
@@ -36,15 +40,29 @@ let gameBoard =  (function (){
 
 })();
 
-function check(gameArr, turn){
+
+
+
+
+function check(){
+  let arr=gameBoard.getBoard();
+
+
+
+
+
+
+
+
+
   for(let i=0; i<3; i++){
     let ocnt=0;
     let xcnt=0;
     for(let j=0; j<3; j++){
-      if(gameArr[i][j]=='O'){
+      if(gameArr[i*3 + j]=='O'){
         ocnt++;
       }
-      if(gameArr[i][j]=='X'){
+      if(gameArr[i*3+j]=='X'){
         xcnt++;
       }
       if(xcnt==3){
@@ -59,10 +77,10 @@ function check(gameArr, turn){
     let ocnt=0;
     let xcnt=0;
     for(let j=0; j<3; j++){
-      if(gameArr[j][i]=='O'){
+      if(gameArr[j*3+i]=='O'){
         ocnt++;
       }
-      if(gameArr[j][i]=='X'){
+      if(gameArr[j*3+i]=='X'){
         xcnt++;
       }
       if(xcnt==3){
@@ -73,23 +91,23 @@ function check(gameArr, turn){
       }
     }
   }
-  if(gameArr[0][0] == gameArr[1][1] && gameArr[1][1] == gameArr[2][2]){
-    if(gameArr[0][0] == 'O'){
+  if(gameArr[0] == gameArr[4] && gameArr[4] == gameArr[8]){
+    if(gameArr[0] == 'O'){
       return 'O';
     }
-    else if(gameArr[0][0] == 'X'){
+    else if(gameArr[0] == 'X'){
       return 'X';
     }
   }
-  if(gameArr[0][2] == gameArr[1][1] && gameArr[1][1] == gameArr[2][0]){
-    if(gameArr[0][2] == 'O'){
+  if(gameArr[2] == gameArr[4] && gameArr[4] == gameArr[6]){
+    if(gameArr[2] == 'O'){
       return 'O';
     }
-    else if(gameArr[0][2] == 'X'){
+    else if(gameArr[2] == 'X'){
       return 'X';
     }
   }
-  if(turn == 9){
+  if(gameBoard.isFull() == true){
     return '-';
 
   }
