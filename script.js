df1dc5421e289e8d25d0d7cc6c3c66310e0de3ca
KarthikChillal <chillalkarthik@gmail.com>
2026-01-30 22:11:21 +0530

    completely written the console working version.

--- script.js
+++ script.js
@@ -4,10 +4,15 @@ function Player(name, marker){
 
 
 
-
-
+//-------------------------------------------------
+// This stores the status of the board
+//-------------------------------------------------
 let gameBoard =  (function (){
   let board =['.', '.', '.', '.', '.', '.', '.', '.', '.', ];
+
+
+
+
   const mark = (marker, index)=>{
     if(board[index]==='.'){
       board[index]= marker;
@@ -18,17 +23,26 @@ let gameBoard =  (function (){
     }
   };
 
+
+
+
   const getBoard = ()=>{
     return board;
   }
+
+
 
   const reset = ()=>{
     board =['.', '.', '.', '.', '.', '.', '.', '.', '.', ];
   };
 
+
+
   const isFull = ()=>{
     return board.every(cell=>cell!== '.');
   }
+
+
 
 
   return {
@@ -43,76 +57,103 @@ let gameBoard =  (function (){
 
 
 
+//----------------------------------------------------------------------------------------------
+//This is for the control of the game, and consists of all the required functions.
+//----------------------------------------------------------------------------------------------
 
-function check(){
-  let arr=gameBoard.getBoard();
-
-
-
-
+const gameController = (() =>{
 
 
 
+  const player1 = Player("Player 1", "X");
+  const player2 = Player("Player 2", "O");
+  let activePlayer = player1;
+  let isGameOver = false;
+
+  const switchPlayerTurn = () => {
+    activePlayer = activePlayer === player1 ? player2 : player1;
+  };
+  const getActivePlayer = () => activePlayer;
 
 
-  for(let i=0; i<3; i++){
-    let ocnt=0;
-    let xcnt=0;
-    for(let j=0; j<3; j++){
-      if(gameArr[i*3 + j]=='O'){
-        ocnt++;
+
+  let gameArr=gameBoard.getBoard();
+
+
+
+  const checkWinner = () => {
+    const winConditions = [
+      [0, 1, 2], [3, 4, 5], [6, 7, 8], // Rows
+      [0, 3, 6], [1, 4, 7], [2, 5, 8], // Columns
+      [0, 4, 8], [2, 4, 6]             // Diagonals
+    ];
+
+    for (let i = 0; i < winConditions.length; i++) {
+      const [a, b, c] = winConditions[i];
+      if (gameArr[a]!='.' && gameArr[a] && gameArr[a] === gameArr[b] && gameArr[a] === gameArr[c]) {
+        return activePlayer.name;
       }
-      if(gameArr[i*3+j]=='X'){
-        xcnt++;
+    }
+    return null;
+  };
+
+
+
+  const checkTie = () => {
+    return gameBoard.isFull();
+  };
+
+
+
+  const playRound = (index)=>{
+    console.log(`current Player: ${activePlayer.name}`);
+    if(isGameOver){
+      console.log('Game is over, Reset.');
+      return;
+    }
+    console.log(`Attempting to place ${activePlayer.marker} at index: ${index}`);
+    const moveSuccess = gameBoard.mark(activePlayer.marker, index);
+    if(moveSuccess){
+      console.log(`Move Successful`);
+      const winner = checkWinner();
+      if(winner != null){
+        isGameOver=true;
+        console.log(`Winner is : ${winner}`);
+        console.log(gameBoard.getBoard());
+        return;
       }
-      if(xcnt==3){
-        return 'X';
+      if(gameBoard.isFull()==true){
+        isGameOver=true;
+        console.log("It's a tie.");
+        console.log(gameBoard.getBoard());
+        return;
       }
-      else if(ocnt==3){
-        return 'O';
-      }
+      switchPlayerTurn();
+      console.log(`Next Player: ${activePlayer.name}`);
+      console.log(gameBoard.getBoard());
+    }
+    else{
+      console.log(`Invalid Move!`);
     }
   }
-  for(let i=0; i<3; i++){
-    let ocnt=0;
-    let xcnt=0;
-    for(let j=0; j<3; j++){
-      if(gameArr[j*3+i]=='O'){
-        ocnt++;
-      }
-      if(gameArr[j*3+i]=='X'){
-        xcnt++;
-      }
-      if(xcnt==3){
-        return 'X';
-      }
-      else if(ocnt==3){
-        return 'O';
-      }
-    }
-  }
-  if(gameArr[0] == gameArr[4] && gameArr[4] == gameArr[8]){
-    if(gameArr[0] == 'O'){
-      return 'O';
-    }
-    else if(gameArr[0] == 'X'){
-      return 'X';
-    }
-  }
-  if(gameArr[2] == gameArr[4] && gameArr[4] == gameArr[6]){
-    if(gameArr[2] == 'O'){
-      return 'O';
-    }
-    else if(gameArr[2] == 'X'){
-      return 'X';
-    }
-  }
-  if(gameBoard.isFull() == true){
-    return '-';
-
-  }
-  return null;
-}
+  return{
+    playRound,
+    checkWinner,
+    getActivePlayer
+  };
 
 
-// console.log(check(gameArr, 5));
+
+
+
+
+})();
+
+gameController.playRound(4);
+gameController.playRound(8);
+gameController.playRound(0);
+gameController.playRound(2);
+gameController.playRound(6);
+gameController.playRound(4);
+gameController.playRound(3);
+// gameController.playRound(5);
